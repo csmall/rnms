56b615500a1b38dc9c1023709e402d05471d2445
Craig Small <csmall@enc.com.au>
2013-05-08 22:38:12 +1000

    Admin controller using jqGrid
    The admin controller can now use the jqGrid, still some things to work
    out like sorting but its working now.

--- rnms/lib/table.py
+++ rnms/lib/table.py
@@ -4,32 +4,49 @@ from sprox.fillerbase import TableFiller
 
 from tw2.jqplugins.jqgrid import jqGridWidget
 
+
 class jqGridGrid(jqGridWidget):
-    args = None
-    params = ['jsId', 'url', 'args','pks','xml_fields','fields',
-              'default_column_width']
-    pks = None
-    url = None
-    jsId = None
-    id = 'hello'
-    default_column_width = '10em'
-    fields = {}
-    pager_options = { 'search': True, 'refresh': True, 'add': False, }
+    id = 'jq-grid-id'
+    params = ['columns', 'column_widths',]
     options = {
-        'pager': 'jqgrid-pager',
-        'dataype': 'json',
-        'viewrecords': True,
-        'imgpath': 'scripts/jgGrid/themes/green/images',
-        'height': 'auto',
-    }
+            'pager': 'hosts-grid-pager',
+            'datatype': 'json',
+            'viewrecords': True,
+            'imgpath': 'scripts/jqGrid/themes/green/images',
+            'width': 900,
+            'height': 'auto',
+            }
+    columns = None
 
     def __init__(self, action, attrs, value):
-        print  'Widget init fields'#, self.fields
-        self.options['url'] = action
-        colwidth =  int(self.default_column_width.strip('em')) * 16
-        self.options['colModel'] = [{'name':
-                                     f[0],'width':colwidth} for f in self.fields]
         super(jqGridGrid, self).__init__()
+        self.options['url'] = action
+        self.options['colNames'] = self._get_colnames()
+        self.options['colModel'] = self._get_colmodel()
+
+    def _get_colnames(self):
+        colnames = []
+        for col in self.columns:
+            try:
+                colnames.append(self.headers[col])
+            except KeyError:
+                colnames.append(col.capitalize())
+        return colnames
+
+    def _get_colmodel(self):
+        colmodel  = []
+        default_width =  self.default_column_width
+        for colname in self.columns:
+            try:
+                width = self.column_widths[colname]
+            except KeyError:
+                width = default_width
+            colmodel.append({
+                'name': colname,
+                'width': width,
+            })
+        return colmodel
+
 
 class jqGridTableBase(TableBase):
     """ A table widget """
@@ -38,47 +55,27 @@ class jqGridTableBase(TableBase):
     __column_options__ = {}
     __retrieves_own_value__ = True
 
-#    def __init__(self, args):
-#        print 'TableBase init'
-#        super(jqGridTableBase, self).__init__(args)
-
-#    def display(self, *args, **kw):
-#        print 'TableBase display'
-#        return super(jqGridTableBase, self).display(*args, **kw)
-
-#    def _do_init_attrs(self):
-#        print 'Tablebase do init attrs '
-#        attrs = super(jqGridTableBase, self)._do_init_attrs()
-#        print 'Tablebase do init attrs ', attrs
-
-#    def _do_get_fields(self):
-#        print 'TableBase do get fields'
-#        fields = super(jqGridTableBase, self)._do_get_fields()
-#        return fields
-
     def _do_get_widget_args(self):
-        print 'TableBase do get widget args start'
         args = super(jqGridTableBase, self)._do_get_widget_args()
         if self.__url__ is not None:
             args['url'] = self.__url__
-        #args['columns'] = self.__fields__
-        #args['column_options'] = self.__column_options__
-        #args['headers'] = self.__headers__
-        args['jsId'] = self.__id__
-        print 'TableBase do get widget args', args
+        args['columns'] = self.__fields__
+        args['column_options'] = self.__column_options__
+        args['headers'] = self.__headers__
         return args
 
 class jqGridTableFiller(TableFiller):
-#    def __init__(self, arg):
-#        print 'table filler init', arg
-#        super(jqGridTableFiller, self).__init__(arg)
-
-    def _calculate_pages(self, offset, limit, records):
-        if offset is None or limit is None or records == 0:
-            return 1,1
-        page =  offset / limit  + 1
-        total = records / limit
-        return page, total
+    def _calculate_pages(self, total_rows, **kw):
+        try:
+            limit = int(kw['rows'])
+        except:
+            limit = None
+        current_page = kw.pop('page', 1)
+        if limit  is None or limit < 1:
+            total_pages = 1
+        else:
+            total_pages = total_rows / limit
+        return current_page, total_pages
 
     def _get_rows(self, items):
         """ Convert the items into rows that jqGrid understands """
@@ -86,7 +83,8 @@ class jqGridTableFiller(TableFiller):
         rows=[]
         for item in items:
             try:
-                rows.append( { 'id': item[identifier], 'cell': item, })
+                rows.append( { 'id': item[identifier],
+                    'cell':[ item[f] for f in self.__fields__] , })
             except IndexError:
                 pass
         return rows
@@ -97,6 +95,7 @@ class jqGridTableFiller(TableFiller):
         kw.pop('sidx', None)
         kw.pop('sord', 'asc')
         kw.pop('nd', False)
+        #FIXME sort orders
         desc =  False
         order_by = None
         if limit is None:
@@ -113,23 +112,11 @@ class jqGridTableFiller(TableFiller):
         return count, objs
 
     def get_value(self, value=None, **kw):
-        print 'before super filler get value'
-        value = super(jqGridTableFiller, self).get_value(value, **kw)
-        print 'after super filler get value'
-        offset = kw.pop('start', None)
-        limit  = kw.pop('count', None)
-        order_by = kw.pop('sort', None)
-        print 'filler get value', offset, limit
-        desc = False
-        if order_by is not None and order_by.startswith('-'):
-            order_by = order_by[1:]
-            desc = True
-        items = super(jqGridTableFiller, self).get_value(
-            value, limit=limit, offset=offset,
-            order_by=order_by, desc=desc, **kw)
-        records = len(items)
-        page, total = self._calculate_pages(offset, limit, records)
-        return {'records': 1, 'total': 1, 'rows': [{'cell': [u'<a href="/hosts/1">No Host</a>', u'Default Zone', 'ttt'], 'id': 1}], 'page': 1}
-        return dict(total=1, page=1, records=1, rows=rows)
+        items = super(jqGridTableFiller, self).get_value(value, **kw)
+        total_records = self.__count__
+        current_page, total_pages = self._calculate_pages(total_records, **kw)
+        rows = self._get_rows(items)
+        return dict(total=total_pages, page=current_page,
+                records=total_records, rows=rows)
 
 
